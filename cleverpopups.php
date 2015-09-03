7f9223c36ac0036f18210758ceb9ea7e7b476e20
organizm <osamborskiy@gmail.com>
2015-09-03 16:04:33 +0300

    New brgin

--- cleverpopups.php
+++ cleverpopups.php
@@ -1,2 +1,11 @@
 <?php
-phpinfo();
+/*
+Plugin Name: Clever PopUps
+Plugin URI: http://wp-funnel
+Description: Clever PopUps
+Version: 3.1.3
+Author: Alex Samborskiy
+Author URI: http://wp-funnel
+License: GPLv2 or later
+Text Domain: cleverpopups
+*/
